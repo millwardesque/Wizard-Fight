7f76d72d7abc75bad8b23ad6e673ed0efb044120
Christopher Millward <christopher.millward@gmail.com>
2015-01-22 16:59:31 -0500

    Added main menu and updated UI scaling and font for all UI elements.  Fixes #14.

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour {
+	public string levelName;
+	
+	public void OnPlayClick() {
+		Application.LoadLevel(levelName);
+	}
+}
